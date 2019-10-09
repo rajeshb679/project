77a3c6a019e5a2221e3c79322b121d4a90169b1f
rajesh <rajeshb679@gmail.com>
2019-10-09 07:56:19 +0530

    Ingredient Model

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,9 @@
+export class Ingredient {
+    /*
+    public name: string;
+    public amount: number;
+    */
+
+    // Direct Creation off class properites through constructor
+    constructor(public name: string, public amount: number) {}
+}
