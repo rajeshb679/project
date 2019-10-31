ccedb776d073b466ce735f617ae0b81dad2d6093
rajesh <rajeshb679@gmail.com>
2019-10-31 22:56:36 +0530

    Using Subject instead of EventEmitter

--- src/app/services/shopping.service.ts
+++ src/app/services/shopping.service.ts
@@ -1,18 +1,21 @@
 import { Injectable, EventEmitter } from '@angular/core';
 
 import { Ingredient } from '../shared/ingredient.model';
+import { Subject } from 'rxjs';
 @Injectable({
     providedIn: 'root',
 })
 export class ShoppingService {
-    addItemEvent = new EventEmitter<any>();
+    // addItemEvent = new EventEmitter<any>();
+    addItemEvent = new Subject<any>();
     private Ingredients: Ingredient[] = [new Ingredient('Apples', 5), new Ingredient('Tomatoes', 10)];
 
     constructor() {}
 
     addItem(ingredient: Ingredient): void {
         this.Ingredients.push(ingredient);
-        this.addItemEvent.emit();
+        // this.addItemEvent.emit();
+        this.addItemEvent.next();
     }
 
     getIngredients(): Ingredient[] {
@@ -21,6 +24,7 @@ export class ShoppingService {
 
     addItems(ingredient: Ingredient[]): void {
         this.Ingredients.push(...ingredient);
-        this.addItemEvent.emit();
+        // this.addItemEvent.emit();
+        this.addItemEvent.next();
     }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,21 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingService } from '../services/shopping.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-shopping-list',
     templateUrl: './shopping-list.component.html',
     styleUrls: ['./shopping-list.component.css'],
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
     Ingredients: Ingredient[];
-
+    private addItemSubscription: Subscription;
     constructor(private shoppingService: ShoppingService) {}
 
     ngOnInit(): void {
         this.Ingredients = this.shoppingService.getIngredients();
-        this.shoppingService.addItemEvent.subscribe(() => {
+        this.addItemSubscription = this.shoppingService.addItemEvent.subscribe(() => {
             this.Ingredients = this.shoppingService.getIngredients();
         });
+    }
+
+    ngOnDestroy(): void {
+        this.addItemSubscription.unsubscribe();
     }
 }
