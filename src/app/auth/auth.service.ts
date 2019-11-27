dd8c308e6187972fd7fa11b9aeda86b9009618b3
Rajesh <rajesh.balla@agilysys.com>
2019-11-27 21:51:40 +0530

    Creating and Storing User Data

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { Observable, throwError, Subject } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { User } from './user.model';
 
 export interface AuthResponseData {
     idToken: string;
@@ -15,6 +16,7 @@ export interface AuthResponseData {
     providedIn: 'root',
 })
 export class AuthService {
+    user = new Subject<User>();
     constructor(private http: HttpClient) {}
 
     singUp(email: string, password: string): Observable<AuthResponseData> {
@@ -27,7 +29,17 @@ export class AuthService {
                     returnSecureToken: true,
                 }
             )
-            .pipe(catchError(this.handleError));
+            .pipe(
+                catchError(this.handleError),
+                tap((authResponse: AuthResponseData) => {
+                    this.handleAuth(
+                        authResponse.email,
+                        authResponse.localId,
+                        authResponse.idToken,
+                        +authResponse.expiresIn
+                    );
+                })
+            );
     }
 
     loginIn(email: string, password: string): Observable<AuthResponseData> {
@@ -40,7 +52,23 @@ export class AuthService {
                     returnSecureToken: true,
                 }
             )
-            .pipe(catchError(this.handleError));
+            .pipe(
+                catchError(this.handleError),
+                tap((authResponse: AuthResponseData) => {
+                    this.handleAuth(
+                        authResponse.email,
+                        authResponse.localId,
+                        authResponse.idToken,
+                        +authResponse.expiresIn
+                    );
+                })
+            );
+    }
+
+    private handleAuth(email: string, id: string, token: string, expies: number): any {
+        const expireData = new Date(new Date().getTime() + +expies * 1000);
+        const user = new User(email, id, token, expireData);
+        this.user.next(user);
     }
 
     private handleError(errorRes: HttpErrorResponse): Observable<any> {
