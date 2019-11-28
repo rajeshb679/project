5ceb4c16c54664367b227f5c4cc286522a1b2a1c
Rajesh <rajesh.balla@agilysys.com>
2019-11-28 07:09:22 +0530

    Adding Auto Login via localStorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth/auth.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    constructor(private authService: AuthService) {}
     title = 'project';
     loadRecipe = false;
     loadShopping = false;
@@ -21,5 +23,9 @@ export class AppComponent {
     feature = 'Recipe';
     onNavigation(feature): void {
         this.feature = feature;
+    }
+
+    ngOnInit(): void {
+        this.authService.autoLogin();
     }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -67,6 +67,23 @@ export class AuthService {
             );
     }
 
+    autoLogin(): void {
+        const userData = JSON.parse(localStorage.getItem('userData'));
+        if (!userData) {
+            return;
+        }
+        const loadedUser = new User(
+            userData.email,
+            userData.id,
+            userData._token,
+            new Date(userData._tokenExpirationDate)
+        );
+
+        if (loadedUser.token) {
+            this.user.next(loadedUser);
+        }
+    }
+
     logout(): void {
         this.user.next(null);
         this.router.navigate(['/login']);
@@ -75,6 +92,7 @@ export class AuthService {
     private handleAuth(email: string, id: string, token: string, expies: number): any {
         const expireData = new Date(new Date().getTime() + +expies * 1000);
         const user = new User(email, id, token, expireData);
+        localStorage.setItem('userData', JSON.stringify(user));
         this.user.next(user);
     }
 
