8ddf6c541eb77d9d1dd12757377e06037955084d
Rajesh <rajesh.balla@agilysys.com>
2019-12-04 10:07:41 +0530

    Dynamic Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/LoadingSpinner/loading-spinner.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import { AlertComponent } from './shared/alert.component';
 
 @NgModule({
     declarations: [
@@ -34,6 +35,7 @@ import { AuthInterceptorService } from './auth/auth-interceptor.service';
         RecipeEditComponent,
         AuthComponent,
         LoadingSpinnerComponent,
+        AlertComponent,
     ],
     imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule, ReactiveFormsModule],
     providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }],
