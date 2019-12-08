a57d1510a76c08f4466bcd2ba537aaa78dcab27c
Rajesh <rajesh.balla@agilysys.com>
2019-12-08 21:18:34 +0530

    Splitting the AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,42 +4,31 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropDownDirectiveDirective } from './shared/drop-down-directive.directive';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/LoadingSpinner/loading-spinner.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { AlertComponent } from './shared/alert.component';
 import { PlaceHolderDirective } from './shared/PlaceHolder/place-holder-directive.directive';
+import { RecipesModule } from './recipes/recipes.module';
 
 @NgModule({
     declarations: [
         AppComponent,
         HeaderComponent,
-        RecipesComponent,
-        RecipeListComponent,
-        RecipeDetailComponent,
-        RecipeItemComponent,
         ShoppingListComponent,
         ShoppingEditComponent,
         DropDownDirectiveDirective,
-        RecipeStartComponent,
-        RecipeEditComponent,
         AuthComponent,
         LoadingSpinnerComponent,
         AlertComponent,
         PlaceHolderDirective,
     ],
-    imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule, ReactiveFormsModule],
+    imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule, ReactiveFormsModule, RecipesModule],
     providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }],
     bootstrap: [AppComponent],
     entryComponents: [AlertComponent],
