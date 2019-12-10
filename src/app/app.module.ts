f8c380d945464190474c6fcc1d5f7a4f2b4730e9
Rajesh <rajesh.balla@agilysys.com>
2019-12-10 07:22:13 +0530

    Having a core module for Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AuthComponent } from './auth/auth.component';
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingModule } from './shopping-list/shopping.module';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
 
 @NgModule({
     declarations: [AppComponent, HeaderComponent, AuthComponent],
@@ -22,8 +22,9 @@ import { SharedModule } from './shared/shared.module';
         SharedModule,
         RecipesModule,
         ShoppingModule,
+        CoreModule,
     ],
-    providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }],
+
     bootstrap: [AppComponent],
 })
 export class AppModule {}
