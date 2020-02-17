ad221c9a7bff82f4ba0022ea66fb5889a72ed0dd
Rajesh Balla <Rajesh.Balla@agilysys.com>
2020-02-17 08:06:03 +0530

    adding action and returning new state

--- src/app/shopping-list/shopping-list.reducer.ts
+++ src/app/shopping-list/shopping-list.reducer.ts
@@ -1,7 +1,16 @@
 import { Ingredient } from '../shared/ingredient.model';
+import { Action } from '@ngrx/store';
 
 const initialState = {
     Ingredients: [new Ingredient('Apples', 5), new Ingredient('Tomatoes', 10)],
 };
 
-export function shoppingListReducer(state = initialState, action) {}
+export function shoppingListReducer(state = initialState, action: Action) {
+    switch (action.type) {
+        case 'ADD_INGREDIENT':
+            return {
+                ...state,
+                ingredient: [...state.Ingredients, action],
+            };
+    }
+}
