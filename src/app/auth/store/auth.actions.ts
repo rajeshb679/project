bd29e72e31c44a265b8aa1716eb623c1cee24ce3
Rajesh Balla <Rajesh.Balla@agilysys.com>
2020-02-19 21:12:03 +0530

    auth actions and reducer function

---
+++ src/app/auth/store/auth.actions.ts
@@ -0,0 +1,17 @@
+import { Action } from '@ngrx/store';
+
+export const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
+
+// ? can't i implete a inteface
+// ? you can but can't initialize the data using constructor..... - Verify
+export class Login implements Action {
+    readonly type = LOGIN;
+    constructor(public payLoad: { email: string; userId: string; token: string; expirationDate: Date }) {}
+}
+
+export class Logout implements Action {
+    readonly type = LOGOUT;
+}
+
+export type AuthActions = Login | Logout;
--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -1,4 +1,6 @@
 import { User } from '../user.model';
+// import { AuthActions } from './auth.actions';
+import * as AuthActions from './auth.actions';
 
 export interface State {
     user: User;
@@ -7,6 +9,23 @@ export interface State {
 const initialState: State = {
     user: null,
 };
-export function authReducer(state = initialState, action) {
+
+export function authReducer(state = initialState, action: AuthActions.AuthActions) {
+    switch (action.type) {
+        case AuthActions.LOGIN:
+            const user = new User(
+                action.payLoad.email,
+                action.payLoad.userId,
+                action.payLoad.token,
+                action.payLoad.expirationDate
+            );
+            // return { ...state, user: user };
+            // if variable name and property name are same we can use like below
+            return { ...state, user };
+        case AuthActions.LOGOUT:
+            return { ...state, user: null };
+        default:
+            return state;
+    }
     return state;
 }
